3990db8046fe2f130b438d0dd71970b44634283c
Sudip Thapa <sthapa@deerhold.org>
2021-09-07 08:39:20 +0545

    components basics completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <MainBody />
+      <Services />
     </div>
-  );
+  )
 }
+
+const Header = () => {
+  return <div id="header">
+      <p>You are presenting to everyone.</p>
+      <p>Stop presenting</p>
+      <Button />
+  </div>
+}
+
+const MainBody = () => {
+  return <div id="main">
+    <h1>Hello World!</h1>
+    <Button />
+  </div>
+}
+
+const Services = () => {
+  return <div id="services">
+    <h3>Services</h3>
+    <div className="services_container">
+      <div className="service">
+        <h4>A</h4>
+      </div>
+      <div className="service">
+        <h4>B</h4>
+      </div>
+      <div className="service">
+        <h4>C</h4>
+      </div>
+    </div>
+  </div>
+}
+
+const Button = () => {
+  return <button className="button">Click Me</button>
+}
+
+
 
 export default App;
