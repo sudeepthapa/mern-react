970f10974156d1dba514c0806c29611a7a26a033
Sudip Thapa <sthapa@deerhold.org>
2021-09-27 08:45:00 +0545

    redux thunk complete

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getAllUsers } from '../store/slices/serviceSlice';
 
 export default function HomePage() {
+
+    const dispatch = useDispatch();
+
+    const { users, status, errorMessage } = useSelector(state=>state.service)
+
+    React.useEffect(() => {
+        dispatch(getAllUsers({id: 21}))
+    }, [dispatch]) 
+    console.log(status)
+    const getUserList = () => {
+        return users.map(user=><h2 key={user.id}>{user.name}</h2>)
+    }
+
+    if(status === 'processing'){
+        return <h1>Loading. Please wait...</h1>
+    }
+    if(status === 'error'){
+        return <h1>{errorMessage}</h1>
+    }
+
     return (
         <div className="main">
-            <h1>Welcome to Redux</h1>
+            { getUserList() }
         </div>
     )
 }
--- src/store/slices/serviceSlice.js
+++ src/store/slices/serviceSlice.js
@@ -1,11 +1,28 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const sliceName = 'services'
+const ENDPOINT_URL = 'https://jsonplaceholder.typicode.com/users';
+
+export const getAllUsers = createAsyncThunk(
+    `${sliceName}/getAllUsers`,
+    async (payload, thunkAPI) => {
+        try{
+            const response = await axios.get(ENDPOINT_URL);
+            return response.data;
+        }catch(err){
+            throw(err)
+        }
+    }
+)
 
 export const serviceSlice = createSlice({
     name: sliceName,
     initialState: {
-        services: []
+        services: [],
+        users: [],
+        status: 'idle',
+        errorMessage: null
     },
     reducers: {
         deleteService: (state, action) => {
@@ -14,6 +31,18 @@ export const serviceSlice = createSlice({
         addService: (state, action) => {
             state.services = [action.payload, ...state.services]
         },
+    },
+    extraReducers: (builder)=>{
+        // getAllUsers
+        builder.addCase(getAllUsers.fulfilled, (state, action)=>{
+            state.users = action.payload
+            state.status = 'success'
+        }).addCase(getAllUsers.pending, (state, action)=>{
+            state.status = 'processing'
+        }).addCase(getAllUsers.rejected, (state, action)=>{
+            state.status = 'error'
+            state.errorMessage = action.error.message
+        })        
     }
 })
 
@@ -25,3 +54,11 @@ export default serviceSlice.reducer;
 // UI -> action dispatch => reducer => state update => UI rerender
 
 // {type: 'services/deleteService', payload: 1}
+
+
+// GIT BASICS
+
+// initialize git in project - git init
+
+// staging - git add <filename> / .
+// commit - git commit -m 'message'
